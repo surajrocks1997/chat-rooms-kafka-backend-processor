629fa07fd5e7316b4d3e47fdfe06737f70a0e1d6
Suraj Gupta <surajrocks1997@gmail.com>
2024-03-17 19:23:37 +0530

    LONG value after processing messages per chat room count serialization added

--- src/main/java/com/example/chatroomskafkabackendprocessor/config/ChatKafkaProcessor.java
+++ src/main/java/com/example/chatroomskafkabackendprocessor/config/ChatKafkaProcessor.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.function.Function;
 
 @Configuration
@@ -19,7 +20,7 @@ import java.util.function.Function;
 public class ChatKafkaProcessor {
 
     @Bean
-    public Function<KStream<String, Message>, KStream<String, String>> aggregate() {
+    public Function<KStream<String, Message>, KStream<String, Long>> aggregateMessagesPerChatRoom() {
         return kStream -> kStream
                 .groupBy((key, value) -> value.getChatRoomName(), Grouped.with(Serdes.String(), new JsonSerde<>(Message.class)))
                 .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(30)))
@@ -28,7 +29,7 @@ public class ChatKafkaProcessor {
                         Materialized.with(Serdes.String(), Serdes.Long()))
                 .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded()))
                 .toStream()
-                .map((key, value) -> new KeyValue<>(key.key(), value.toString()))
-                .peek((key, value) -> log.info("Aggregated Data: Key: {}   -   Value: {}", key, value));
+                .map((key, value) -> new KeyValue<>(key.key(), value))
+                .peek((key, value) -> log.info("Aggregated Messages Per Chat Room: Key: {}   -   Value: {}", key, value));
     }
 }
